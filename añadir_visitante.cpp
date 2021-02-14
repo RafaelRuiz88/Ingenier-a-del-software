1a25c0b5717c477e2d430f332b922c8311538337
RafaelRuiz88 <71658870+RafaelRuiz88@users.noreply.github.com>
2021-02-14 14:56:26 +0100

    Añadir visitante(segunda versión)
    Este commit corresponde  a la segunda versión de la implementación de la historia de usuario "Añadir visitante a ruta", es la versión resultante de aplicar las soluciones a los errores encontrados durante la realización de las pruebas unitarias.

--- añadir_visitante.cpp
+++ añadir_visitante.cpp
@@ -10,18 +10,38 @@ int main(){
 	string contenido;
 	string buscar;      // texto a buscar
   	string visitante;  // reemplazar por
+  	int entero; 		// Pare asegurarse que la entrada sea un entero
 	int i=0;
+	int busca=0;
 	
+	bool valido=true;
 	bool cambio=false;
 	bool lista =false;
+	bool encontrado = false;
 	
  	 //ingresa textos
-	cout << "Ingresa el identificador de la ruta: ";
-	//cin.ignore();
-	getline(cin, buscar);
+	while (busca <1000 || busca >9999){
+		cout << "Ingresa el identificador de la ruta: ";
+		cin >> busca; 
+		if (busca==0){
+			cout << "Error, debe ser un valor numérico" << endl; 
+			break;
+			}
+		if(busca <1000 || busca >9999){
+			cout << "Valor incorrecto" << endl;
+		}
+	}
+	buscar=to_string(busca);
+	
 	cout << "Ingresa el DNI(sin letra) del visitante: ";
-	//cin.ignore();
-	getline(cin, visitante);
+	cin >> entero;
+	
+	if (entero<10000000 || entero>99999999){
+		cout << "Entrada no válida" << endl; 
+		valido=false;
+	}
+	
+	visitante = to_string(entero); 
 	
 	ifstream fs("Registro_rutas.txt"); //leer de este archivo
 	ofstream fstemp("nombretemp.txt"); //escribir en este archivo
@@ -34,14 +54,16 @@ int main(){
   	while(fs >> contenido)
   	{
   		if(lista==true){
-  			contenido=contenido+visitante; 
+  			contenido=contenido+visitante+ "-"; 
   			lista=false; 
   			cambio=false; 
+ 
   		
   		}
   		if(contenido == buscar){  //se encontro
   		
   			cambio = true; 
+  			encontrado=true;
       			}
       		if (cambio ==true && contenido== "lista"){
       			lista = true; 
@@ -49,7 +71,10 @@ int main(){
       		
     	fstemp << contenido << endl;
   	}
-
+	
+	if (cambio ==false){
+  		cout << "No encontrado" << endl;
+  	}
   	//reemplazar un archivo por otro
   	fs.close();
   	fstemp.close();
@@ -57,6 +82,7 @@ int main(){
   	rename("nombretemp.txt", "Registro_rutas.txt");  // renombrar el temporal
 
 
+  	if (valido==true)
   	cout << "El archivo ha sido modificado correctamente" << endl;
   	system("pause");
 }
